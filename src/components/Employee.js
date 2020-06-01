ecf3a6ff3dbf55204d5b178a472fb4630c2012c1
Kaden Maguglin <kadenmaguglin@gmail.com>
2020-06-01 13:02:15 -0700

    final touches

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 import EmployeeTable from "./EmployeeTable";
 
 class Employee extends React.Component{
@@ -7,33 +7,10 @@ state = {
     employeeRecords:[]
 }
 
-    componentDidMount =()=>{
-        axios.get("https://randomuser.me/api/?results=10&nat=us")
-        .then(records=>{
-            console.log(records);
-            var employeeRecords = []
-            var employeeData = records.data.results
-
-            for (let i = 0; i < employeeData.length; i++) {
-                let data = {
-                    firstName: employeeData[i].name.first,
-                    lastName: employeeData[i].name.last,
-                    phoneNumber: employeeData[i].cell,
-                    location: employeeData[i].location.city,
-                    email: employeeData[i].email
-                };
-                employeeRecords.push(data)
-            }
-            this.setState({
-                employeeRecords:employeeRecords
-            })
-            console.log(this.state.employeeRecords);
-        })
-    }
 
     render(){
         return(<div>
-            <h1>Employee Directory: </h1>
+            <h1 id="head">Employee Directory: </h1>
             <EmployeeTable rows={this.state.employeeRecords}></EmployeeTable>
         </div>)
     }
--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { MDBDataTable } from "mdbreact";
+import axios from "axios";
 
 class EmployeeTable extends React.Component {
     state = {
-        data: {
             columns: [
                 {
                     label: "firstName",
@@ -42,13 +42,42 @@ class EmployeeTable extends React.Component {
                 }
 
             ],
-            rows: this.props.rows
-        }
+            rows: []
+            
+        
     }
+    componentDidMount =()=>{
+        axios.get("https://randomuser.me/api/?results=200&nat=us")
+        .then(records=>{
+            console.log(records);
+            var employeeRecords = []
+            var employeeData = records.data.results
+
+            for (let i = 0; i < employeeData.length; i++) {
+                let data = {
+                    firstName: employeeData[i].name.first,
+                    lastName: employeeData[i].name.last,
+                    phoneNumber: employeeData[i].cell,
+                    location: employeeData[i].location.city,
+                    email: employeeData[i].email
+                };
+                employeeRecords.push(data)
+            }
+            this.setState({
+                rows:employeeRecords
+            })
+            console.log(this.state.employeeRecords);
+        })
+    }
+
     render() {
         console.log(this.props.rows);
+        var data = {
+            columns: this.state.columns,
+            rows: this.state.rows
+        }
         return (
-            <MDBDataTable striped bordered hover data={this.state.data} />
+            <MDBDataTable striped bordered hover data = {data} class="table"/>
         )
     }
 }
